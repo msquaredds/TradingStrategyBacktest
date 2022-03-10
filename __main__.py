3ab062ae3379ec5886b014c45629dc8cd60da37e
amelesko <97187441+amelesko@users.noreply.github.com>
2022-03-10 11:20:38 -0500

    Update __main__.py
    Printing RandomForest option types to debug

--- __main__.py
+++ __main__.py
@@ -254,9 +254,9 @@ def main():
     st.markdown("Do this after (re-)running options above for the RandomForest, "
         "or re-running the factors so that you can access the new probabilities.")
     st.markdown("Note that this can take up to 15 minutes to run.")
-    st.write(tree_count_choice)
-    st.write(node_count_choice)
-    st.write(dependent_pca_choice)
+    st.write(type(tree_count_choice))
+    st.write(type(node_count_choice))
+    st.write(type(dependent_pca_choice))
     st.button('Run RandomForest', on_click=front_end_callbacks.update_randomforest,
         args=(tree_count_choice, node_count_choice, dependent_pca_choice))    
         
