37f894a5a4fbad48e991575c6036064e515ba3cb
amelesko <97187441+amelesko@users.noreply.github.com>
2022-03-23 11:19:17 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -440,11 +440,10 @@ def main():
             strategy_comparison_choice = st.multiselect("Would you like to compare against "
                 "any of the futures?", plain_english_rets_columns,
                 default=("S&P 500", "10 Year Treasuries"))
-            st.write(strategy_comparison_choice)
-            st.write(list(mapping_dict_rets.values()))
             chart_result_column = [k for k, v in mapping_dict_rets.items() if v in strategy_comparison_choice]
             #chart_result_column = list(mapping_dict_rets.keys())[list(mapping_dict_rets.values()).index(strategy_comparison_choice)]
             st.write(chart_result_column)
+            st.write(mapping_dict_rets)
             fig_strat = backtest.plot_strat(chart_result_column, mapping_dict_rets)
             st.plotly_chart(fig_strat)
 
