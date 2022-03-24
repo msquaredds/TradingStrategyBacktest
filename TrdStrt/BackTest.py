5593ee8cd7e1931ae8704024caa9b963d4952168
amelesko <97187441+amelesko@users.noreply.github.com>
2022-03-23 21:08:20 -0400

    Update BackTest.py

--- TrdStrt/BackTest.py
+++ TrdStrt/BackTest.py
@@ -1159,12 +1159,6 @@ class BackTest(ts.HelperFunctions):
         index_name = 'Metrics'
         self.metrics = pd.DataFrame(columns=metric_names)
         self.metrics.loc[index_name] = temp_metrics
-        
-        # Style the metrics for output
-        self.metrics.style.format(formatter={'Lookback':"{:.0f}",
-            'Horizon':"{:.0f}",'Average':"{:.4f}",'Std_Dev':"{:.4f}",'Skew':"{:.4f}",
-            'Sharpe':"{:.4f}",'Drawdown':"{:.2%}",'Corr_to_SandP':"{:.2%}"},
-            axis=1)
       
     def _create_index(self, future_to_use, plain_english_name, input_df):
         '''
